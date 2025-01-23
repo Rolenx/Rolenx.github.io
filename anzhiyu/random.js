88d1975c10082eaf3c0e2c988d80f9ad0d61ae39
Mikazuki Tomoko <142188839+Yesord@users.noreply.github.com>
2025-01-24 02:07:47 +0800

    Site updated: 2025-01-24 02:07:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/06/welcome/","2024/11/06/Hello/"];function toRandomPost(){
+var posts=["2024/11/06/welcome/","2024/11/06/Hello/","2025/01/23/入坑机娘浅谈/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
